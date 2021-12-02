98fc7690a5eb6e65e4f7105233276c471bcefabe
Diogo Duarte <dduarte@odoogap.com>
2021-12-02 11:58:42 +0000

    [IMP] added code inspection

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tracks Price History on Odoo
+
+## Instalation
+
+Just install and search for a menu "Inventory >> Master Data >> Price History"
+
+Add the following System Param to get additional information:
+
+key: price_history.code_context
+value: 1
--- models/product.py
+++ models/product.py
@@ -2,6 +2,8 @@ from odoo import api, fields, models, _
 from odoo.tools.float_utils import float_compare
 import json
 import logging
+import inspect
+from pprint import pprint
 
 _logger = logging.getLogger(__name__)
 
@@ -12,6 +14,7 @@ class ProductProduct(models.Model):
     name = fields.Text(string="Vals", required=True)
     value = fields.Float(string='Sale Base Price', digits='Product Price', required=True, default=0.0)
     product_id = fields.Many2one('product.product', 'Product', required=True)
+    code_context = fields.Text(string="Code context", required=True)
 
 
 class ProductProduct(models.Model):
@@ -19,14 +22,22 @@ class ProductProduct(models.Model):
 
     def write(self, vals):
         if 'standard_price' in vals:
+            code_context = self.env["ir.config_parameter"].sudo().get_param("price_history.code_context", False)
             standard_price = vals['standard_price']
+            data = {
+                'name': json.dumps(vals),
+                'value': standard_price,
+                'product_id': self.id
+            }
+            if code_context:
+                stack_trace = ""
+                for frame in inspect.stack()[1:]:
+                    stack_trace += "# %s\n%s\n%s" % (
+                        frame.filename, "-" * 50, frame.code_context and frame.code_context[0] or 'N/A')
+                data.update({'code_context': stack_trace})
             # if negative we remove it but will log
             if float_compare(standard_price, 0.0, precision_digits=3) < 0.0:
                 _logger.info("Prices cannot be negative")
                 vals.pop('standard_price')
-            self.env['product.product.history'].create({
-                'name': json.dumps(vals),
-                'value': standard_price,
-                'product_id': self.id
-            })
+            self.env['product.product.history'].create(data)
         return super(ProductProduct, self).write(vals)
